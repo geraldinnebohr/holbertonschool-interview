cfd1f401faf4d6d6023879f009fc7caf5f525d37
geraldinnebohr <geraldinnebohr@gmail.com>
2020-06-03 23:40:20 -0500

    [FIX] ff

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -29,21 +29,21 @@ try:
         if len(args) > 2:
             status_line = int(args[-2])
             file_size = args[-1]
-            if status_line in status_code:
-                status_code[status_line] += 1
+            status_code[status_line] += 1
             sum_file_size += int(file_size)
             i += 1
-            if i % 10 == 0:
-                print('File size: {}'.format(sum_file_size))
+            if i == 10:
+                print('File size: {:d}'.format(sum_file_size))
                 sorted_keys = sorted(status_code.keys())
                 for key in sorted_keys:
                     value = status_code[key]
                     if value != 0:
                         print('{}: {}'.format(key, value))
+                i = 0
 except Exception:
     pass
 finally:
-    print('File size: {}'.format(sum_file_size))
+    print('File size: {:d}'.format(sum_file_size))
     sorted_keys = sorted(status_code.keys())
     for key in sorted_keys:
         value = status_code[key]

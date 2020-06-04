bc381bc147d1fb13e044e14371b8177f18befd9d
geraldinnebohr <geraldinnebohr@gmail.com>
2020-06-03 22:54:14 -0500

    [FIX] sorted status cod

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 """script that reads stdin line by line and computes metrics"""
 
-import fileinput
-import signal
 import sys
 
-
-def signal_handling(signum, frame):
-    """handle ctrl + c keyboard interruption signal"""
-    print('File size: {}'.format(sum_file_size))
-    for key, value in status_code.items():
-        if value != 0:
-            print('{}: {}'.format(key, value))
 
 sum_file_size = 0
 status_code = {200: 0,
@@ -23,18 +14,32 @@ status_code = {200: 0,
                405: 0,
                500: 0}
 
-i = 0
-signal.signal(signal.SIGINT, signal_handling)
-for line in fileinput.input():
-    args = line.split()
-    status_line = int(args[-2])
-    file_size = args[-1]
-    status_code[status_line] += 1
-    sum_file_size += int(file_size)
-    i += 1
-    if i % 10 == 0:
-        terminate = False
-        print('File size: {}'.format(sum_file_size))
-        for key, value in status_code.items():
-            if value != 0:
-                print('{}: {}'.format(key, value))
+
+def signal_handling():
+    """handle ctrl + c keyboard interruption signal"""
+    print('File size: {}'.format(sum_file_size))
+    sorted_keys = sorted(status_code.keys())
+    for key in sorted_keys:
+        value = status_code[key]
+        if value != 0:
+            print('{}: {}'.format(key, value))
+
+try:
+    i = 0
+    for line in sys.stdin:
+        args = line.split()
+        status_line = int(args[-2])
+        file_size = args[-1]
+        status_code[status_line] += 1
+        sum_file_size += int(file_size)
+        i += 1
+        if i % 10 == 0:
+            terminate = False
+            print('File size: {}'.format(sum_file_size))
+            for key, value in status_code.items():
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+except Exception:
+    pass
+finally:
+    signal_handling()

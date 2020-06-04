428aa58cca8e60e6d49514b08e3b14d61e56fd73
geraldinnebohr <geraldinnebohr@gmail.com>
2020-06-04 00:06:41 -0500

    [FIX] ff

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,22 +6,23 @@ import sys
 
 i = 0
 sum_file_size = 0
-status_code = {200: 0,
-               301: 0,
-               400: 0,
-               401: 0,
-               403: 0,
-               404: 0,
-               405: 0,
-               500: 0}
+status_code = {'200': 0,
+               '301': 0,
+               '400': 0,
+               '401': 0,
+               '403': 0,
+               '404': 0,
+               '405': 0,
+               '500': 0}
 
 try:
     for line in sys.stdin:
         args = line.split(' ')
         if len(args) > 2:
-            status_line = int(args[-2])
+            status_line = args[-2]
             file_size = args[-1]
-            status_code[status_line] += 1
+            if status_line in status_code:
+                status_code[status_line] += 1
             sum_file_size += int(file_size)
             i += 1
             if i == 10:

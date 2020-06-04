b28c3fec8520ec0fcc7a9d360b213c9d1fcf83e6
geraldinnebohr <geraldinnebohr@gmail.com>
2020-06-03 23:21:55 -0500

    [FIX] ff

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -29,7 +29,8 @@ try:
         if len(args) > 2:
             status_line = int(args[-2])
             file_size = args[-1]
-            status_code[status_line] += 1
+            if status_line in status_code:
+                status_code[status_line] += 1
             sum_file_size += int(file_size)
             i += 1
             if i % 10 == 0:
